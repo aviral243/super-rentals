ce0f1a4f5743a2321683ae7b2e357a5b2999341e
aviral243 <aviralgangwar24@gmail.com>
2019-12-15 18:15:50 +0530

    Model hook for rentals

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -21,7 +21,7 @@ module("Acceptance | list rentals", function(hooks) {
     await click(".menu-about");
     assert.equal(currentURL(), "/contact", "should navigate to contact");
   });
-  test("should list avaialble rentals", async function(assert) {});
-  test("should filtet the list of rentals by city", async function(assert) {});
-  test("should show details for a selected rentals", async function(assert) {});
+  // test("should list avaialble rentals", async function(assert) {});
+  // test("should filtet the list of rentals by city", async function(assert) {});
+  // test("should show details for a selected rentals", async function(assert) {});
 });
