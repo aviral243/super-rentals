54c48e165ef21311efa2b9d74c9c4ac29c00ab09
aviral243 <aviralgangwar24@gmail.com>
2019-12-15 15:20:49 +0530

    Wrote tests

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,14 @@
+import { module, test } from "qunit";
+import { visit, currentURL } from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
+
+module("Acceptance | list rentals", function(hooks) {
+  setupApplicationTest(hooks);
+
+  test("should show rentals as the home page", async function(assert) {});
+  test("should link to information about the company", async function(assert) {});
+  test("should link to contact information", async function(assert) {});
+  test("should list avaialble rentals", async function(assert) {});
+  test("should filtet the list of rentals by city", async function(assert) {});
+  test("should show details for a selected rentals", async function(assert) {});
+});
